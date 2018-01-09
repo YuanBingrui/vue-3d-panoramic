2fda1cf310570c32891ce3634a519c8b9a7aacd3
袁秉瑞 <bingrui.yuan@roadshare.com>
2018-01-09 18:14:50 +0800

    some modification

--- src/main.js
+++ src/main.js
@@ -2,10 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import { Button, Input, Upload, Dialog, Message, Dropdown, DropdownMenu, DropdownItem } from 'element-ui'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Upload)
+Vue.use(Dropdown)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+Vue.use(Dialog)
+Vue.use(Message)
+
+Vue.prototype.$message = Message
+
 new Vue({
   el: '#app',
   template: '<App/>',
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="home">
-    <div class="plainImgBox">
-      
+    <div class="contentBox">
+      <div class="leftBox titleFontSize">平面图</div>
+      <div class="rightBox titleFontSize">3D全景</div>
     </div>
-    <div class="panoramaBox">
-      <ThreeDshow :cubeImg="cubeImg"></ThreeDshow>
+    <div class="contentBox">
+      <div class="leftBox plainImgBox">
+        <el-upload
+          action=""
+          list-type="picture-card"
+          :on-preview="handlePictureCardPreview"
+          :on-remove="handleRemove"
+          :on-change="handleChange"
+          :auto-upload="false"
+          :limit="6"
+          :on-exceed="handleExceed"
+          class="upload-card">
+          <i class="el-icon-plus"></i>
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+          <img width="100%" :src="dialogImageUrl" alt="">
+        </el-dialog>
+      </div>
+      <div class="rightBox">
+        <ThreeDshow :cubeImg="cubeImg"></ThreeDshow>
+      </div>
     </div>
   </div>
 </template>
@@ -18,17 +38,40 @@ export default {
   },
   data () {
     return {
+      dialogImageUrl: '',
+      dialogVisible: false,
       cubeImg: [
         '../../static/cube/posx.jpg', '../../static/cube/negx.jpg',
         '../../static/cube/posy.jpg', '../../static/cube/negy.jpg',
         '../../static/cube/posz.jpg', '../../static/cube/negz.jpg'
       ]
     }
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file)
+      console.log(fileList)
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
+    handleChange(file, fileList) {
+      if(fileList.length === 6){
+        this.cubeImg.forEach((value, index) => {
+          this.cubeImg[index] = fileList[index].url
+        })
+      }
+      console.log(this.cubeImg)
+      console.log(fileList)
+    },
+    handleExceed(files, fileList){
+      this.$message.warning(`只能上传${fileList.length}个文件`);
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less">
   @import "./home.less";
 </style>
